b1518d4ff249382f772f35fba5fa460fdb8d133c
dorrang <summ.pie@gmail.com>
2022-08-23 16:12:05 +0300

    code page

--- src/views/get-started.vue
+++ src/views/get-started.vue
@@ -2,15 +2,24 @@
   <div class="get-started">
     <img class="saveme-logo" alt="SaveMe" src="../assets/imgs/logo.png" />
 
-    <div class="get-container">
+    <div v-if="gotCode" class="get-container">
+      <div class="code-container">
+        <p>נא להזין את הקוד שהתקבל</p>
+        <input class="partitioned" type="text" maxlength="6" autofocus />
+      </div>
+    </div>
+
+    <div v-else class="get-container">
       <img src="../assets/imgs/hand-icon.png" alt="" />
       <h3 class="title">שמחים לפגוש אותך!</h3>
-      <p>
+      <p class="content">
         תהליך הזדהות קצר ואפשר להתחיל לחסוך באשראי, בקלות בהנאה ובמגוון אפשרויות
         השקעה.
       </p>
       <form class="auth-form">
+        <p class="txt-lable">ת.ז /דרכון</p>
         <input class="txt-input" type="number" name="" />
+        <p class="txt-lable">6 ספרות אחרונות של כרטיס ישראכרט</p>
         <input class="txt-input" type="number" name="" />
 
         <h4>איך לשלוח לך קוד הזדהות?</h4>
@@ -29,8 +38,7 @@
           </div>
         </div>
       </form>
-
-      <div class="btn">שלחו לי קוד</div>
+      <div class="btn" @click="getCode()">שלחו לי קוד</div>
     </div>
   </div>
 </template>
@@ -38,5 +46,15 @@
 <script>
 export default {
   name: "get-started",
+  data() {
+    return {
+      gotCode: false,
+    };
+  },
+  methods: {
+    getCode() {
+      this.gotCode = !this.gotCode;
+    },
+  },
 };
 </script>

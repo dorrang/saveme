f94c9e66298bf19c940860a7bb4186fb03be71cf
dorrang <summ.pie@gmail.com>
2022-08-24 20:40:37 +0300

    events and add new event comps

--- src/components/create-event.vue
+++ src/components/create-event.vue
@@ -1,9 +1,35 @@
 <template>
-  <div></div>
+  <div class="new-ev-container">
+    <img
+      class="event-img"
+      :src="require(`../assets/imgs/${ev.imgUrl}.png`)"
+      alt=""
+    />
+    <h3 class="title">{{ ev.eventName }}</h3>
+
+    <form class="event-form">
+      <p class="txt-lable">תיאור היעד</p>
+      <input class="txt-input" type="text" name="" />
+      <p class="txt-lable">כמה היעד יעלה</p>
+      <input class="txt-input" type="number" />
+      <p class="txt-lable">למתי צריך את הכסף</p>
+      <input class="txt-input" type="month" placeholder="חודש / שנה" name="" />
+    </form>
+    <div class="btn">שמור יעד</div>
+  </div>
 </template>
 
 <script>
 export default {
   name: "create-event",
+  props: { ev: Object },
+  data() {
+    return {
+      newEv: {},
+    };
+  },
+  mounted() {
+    console.log(this.ev);
+  },
 };
 </script>
--- src/views/events.vue
+++ src/views/events.vue
@@ -1,6 +1,40 @@
 <template>
   <div>
-    <create-event />
+    <div class="events-container">
+      <create-event v-if="chosenEv" :ev="chosenEv" />
+      <div v-else>
+        <h3>אלו יעדים ברצונך להגשים?</h3>
+        <div class="events-grid">
+          <div class="grid-item" @click="newEv('טיסה', 'plane-icon')">
+            <img src="../assets/imgs/plane-icon.png" alt="" />
+            <p class="grid-item-desc">טיסה</p>
+          </div>
+          <div class="grid-item" @click="newEv('טיול', 'tree-icon')">
+            <img src="../assets/imgs/tree-icon.png" alt="" />
+            <p class="grid-item-desc">טיול</p>
+          </div>
+          <div class="grid-item" @click="newEv('לימודים', 'student-icon')">
+            <img src="../assets/imgs/student-icon.png" alt="" />
+            <p class="grid-item-desc">לימודים</p>
+          </div>
+          <div class="grid-item" @click="newEv('רכב', 'car-icon')">
+            <img src="../assets/imgs/car-icon.png" alt="" />
+            <p class="grid-item-desc">רכב</p>
+          </div>
+          <div class="grid-item" @click="newEv('אירוע', 'cake-icon')">
+            <img src="../assets/imgs/cake-icon.png" alt="" />
+            <p class="grid-item-desc">אירוע</p>
+          </div>
+          <div
+            class="grid-item"
+            @click="newEv('חיסכון לילד', 'wheelcart-icon')"
+          >
+            <img src="../assets/imgs/wheelcart-icon.png" alt="" />
+            <p class="grid-item-desc">חיסכון לילד</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -10,5 +44,19 @@ import createEvent from "../components/create-event.vue";
 export default {
   components: { createEvent },
   name: "events",
+  data() {
+    return {
+      chosenEv: null,
+    };
+  },
+  methods: {
+    newEv(evName, evImg) {
+      this.chosenEv = {
+        eventName: evName,
+        imgUrl: evImg,
+      };
+      //   console.log(evName);
+    },
+  },
 };
 </script>

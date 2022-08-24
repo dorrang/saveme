405492b2ba844a287806bbefe89c3bdc3ad2cbbd
dorrang <summ.pie@gmail.com>
2022-08-24 16:38:11 +0300

    pref

--- src/components/pref.vue
+++ src/components/pref.vue
@@ -4,42 +4,52 @@
     <img src="../assets/imgs/card.png" alt="" />
     <form>
       <div>
-        <div>
-          <input
-            class="checkbox"
-            type="checkbox"
-            name="round-up"
-            value="round-up"
-            checked
-          />
-          <p for="round-up">עיגול כלפי מעלה בכל חיוב</p>
-          <div>
+        <div class="content-container">
+          <div class="section-container">
+            <input
+              class="checkbox"
+              type="checkbox"
+              name="round-up"
+              value="round-up"
+              checked
+            />
+
+            <p for="round-up">עיגול כלפי מעלה בכל חיוב</p>
+          </div>
+          <div class="round-up">
             <input class="radio-btn" type="radio" name="2" />
             <label for="2">2 ש"ח</label>
-            <input class="radio-btn" type="radio" name="5" />
+            <input class="radio-btn" type="radio" name="5" checked />
             <label for="5">5 ש"ח</label>
             <input class="radio-btn" type="radio" name="10" />
             <label for="10">10 ש"ח</label>
           </div>
         </div>
         <hr />
-        <input type="checkbox" class="checkbox" checked />
-        <p class="rtl">חיסכון חודשי קבוע של {{ monthlySavings }} ש"ח</p>
-        <input
-          class="ltr"
-          type="range"
-          name=""
-          id=""
-          min="0"
-          max="500"
-          v-model="monthlySavings"
-          value="0"
-          step="1"
-        />
+        <div class="content-container">
+          <div class="section-container">
+            <input type="checkbox" class="checkbox" checked />
+            <p class="rtl">חיסכון חודשי קבוע של {{ monthlySavings }} ש"ח</p>
+          </div>
+          <input
+            class="ltr"
+            type="range"
+            name=""
+            id=""
+            min="0"
+            max="500"
+            v-model="monthlySavings"
+            value="0"
+            step="1"
+          />
+        </div>
         <hr />
-
-        <input type="checkbox" class="checkbox" checked />
-        <label>הנחות במעמד החיוב. <a href="">קרא עוד +</a></label>
+        <div class="content-container">
+          <div class="section-container">
+            <input type="checkbox" class="checkbox" checked />
+            <label>הנחות במעמד החיוב. <a href="">קרא עוד +</a></label>
+          </div>
+        </div>
       </div>
     </form>
     <div class="btn">המשך</div>

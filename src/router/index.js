6bb16eb79086a13145a871d2e60c89a0bf204811
dorrang <summ.pie@gmail.com>
2022-08-24 18:37:08 +0300

    events comps

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import getStarted from '../views/get-started.vue'
+import events from '../views/events.vue'
 
 Vue.use(VueRouter)
 
@@ -23,6 +24,11 @@ const routes = [
     path: '/get-started',
     name: 'Get Started',
     component: getStarted
+  },
+  {
+    path: '/events',
+    name: 'Events',
+    component: events
   }
 ]
 
---
+++ src/views/events.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <create-event />
+  </div>
+</template>
+
+<script>
+import createEvent from "../components/create-event.vue";
+
+export default {
+  components: { createEvent },
+  name: "events",
+};
+</script>
